543395c067dc09ac11f502845c4a001de44e8b8b
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-04 09:07:05 -0300

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import Home from 'pages/Home'
+import Routes from './routes'
 
 import './App.css'
 
 function App() {
   return (
-    <Home />
+    <Routes />
   )
 }
 
